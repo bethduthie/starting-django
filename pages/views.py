e6e34761ce5797d5c5445815ad39718810c21cec
bethduthie <bethanyduthie@gmail.com>
2020-05-19 16:44:51 +0100

    Learned about implementing templates and basic forms

--- pages/views.py
+++ pages/views.py
@@ -11,4 +11,8 @@ def contact_view(request, *args, **kwargs):
     return render(request, 'contact.html', {})
 
 def about_view(request, *args, **kwargs):
-    return render(request, 'about.html', {})
+    my_context = {'my_text': "this is about us",
+                  'this_is_true': True,
+                  'my_number': 123,
+                  'my_list': [123, 1212, 312]}
+    return render(request, 'about.html', my_context)
--- products/models.py
+++ products/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+    def __repr__(self):
+        return '{} {}'.format(self.title, self.id).title()
+
     title = models.CharField(max_length=120) # must define a max length
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=1000)
     summary = models.TextField()
-    featured = models.BooleanField()
+    featured = models.BooleanField(default=False)
